a0bbc42a963d9b2403f91197912d990715501959
Jobins John <jobins666@gmail.com>
2019-02-13 12:46:35 +0400

    Initial Commit

---
+++ AutoScroll/AutoScroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoScroll
+//
+//  Created by Jobins John on 2/13/19.
+//  Copyright © 2019 Jobins John. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
